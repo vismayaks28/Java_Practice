55f1d3fd87cbbef1daa0200ba8f52e7a3327c658
Vismayaks28 <vismayaksofficial@gmail.com>
2026-05-17 20:43:01 +0530

    refact: main method logic has been changed

--- src/com/lpu/practice/HelloWorld.java
+++ src/com/lpu/practice/HelloWorld.java
@@ -3,5 +3,7 @@ package com.lpu.practice;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello world");
+        int age = 21;
+        String name = "vimaya";
     }
 }
